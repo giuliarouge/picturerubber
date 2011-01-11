644c19ae928bf1533d399d4d58c0b487ba92c3c5
Stefan.Benischke <Stefan.Benischke@efc28e42-3d81-718d-be2e-65ac8396be35>
2011-01-11 12:47:21 +0000

    Menu improvements

--- trunk/PictureRubber/PictureRubber/PR_MainMenu.cs
+++ trunk/PictureRubber/PictureRubber/PR_MainMenu.cs
@@ -26,10 +26,16 @@ namespace PictureRubber
         private PR_InputManager m_InputManager;
 
         public bool m_Visible;
+        private bool[] m_SelectedIndex;
 
         public PR_MainMenu(PR_Main _root, PR_InputManager _input)
         {
             this.m_Visible = true;
+            this.m_SelectedIndex = new bool[4];
+            this.m_SelectedIndex[0] = false;
+            this.m_SelectedIndex[1] = false;
+            this.m_SelectedIndex[2] = false;
+            this.m_SelectedIndex[3] = false;
             this.m_Root = _root;
             this.m_InputManager = _input;
             this.m_Background = this.m_Root.Content.Load<Texture2D>("menu\\background");
@@ -68,10 +74,19 @@ namespace PictureRubber
             this.m_BeendenButtonRect = new Rectangle((int)(196 * value), (int)(972 * value), (int)(this.m_StartenButton[0].Width * value), (int)(this.m_StartenButton[0].Height * value));
         }
 
+        public void Update(GameTime _gameTime)
+        {
+            if (this.m_InputManager.GetMouseState().LeftButton == Microsoft.Xna.Framework.Input.ButtonState.Released)
+            {
+                //this.m_SelectedIndex.Co
+            }
+        }
+
         public void Draw(GameTime _gameTime)
         {
             if (this.m_Visible)
             {
+                Vector2 mousePosition = this.m_InputManager.GetMousePosition();
                 this.m_Root.m_SpriteBatch.Begin();
                 Rectangle rec = new Rectangle(0, 0, this.m_Root.GraphicsDevice.Viewport.Width, this.m_Root.GraphicsDevice.Viewport.Height);
                 this.m_Root.m_SpriteBatch.Draw(m_Background, rec, Microsoft.Xna.Framework.Color.White);
@@ -79,12 +94,82 @@ namespace PictureRubber
                 this.m_Root.m_SpriteBatch.Draw(m_RightLine, rec, Microsoft.Xna.Framework.Color.White);
                 this.m_Root.m_SpriteBatch.Draw(m_Rubber, rec, Microsoft.Xna.Framework.Color.White);
                 this.m_Root.m_SpriteBatch.Draw(m_MenuFrame, rec, Microsoft.Xna.Framework.Color.White);
-                this.m_Root.m_SpriteBatch.Draw(m_StartenButton[0], m_StartButtonRect, Microsoft.Xna.Framework.Color.White);
-                this.m_Root.m_SpriteBatch.Draw(m_FotowandButton[0], m_FotowandButtonRect, Microsoft.Xna.Framework.Color.White);
-                this.m_Root.m_SpriteBatch.Draw(m_OptionenButton[0], m_OptionenButtonRect, Microsoft.Xna.Framework.Color.White);
-                this.m_Root.m_SpriteBatch.Draw(m_BeendenButton[0], m_BeendenButtonRect, Microsoft.Xna.Framework.Color.White);
+                if (Intersects(mousePosition, m_StartButtonRect))
+                {
+                    if (this.m_InputManager.GetMouseState().LeftButton == Microsoft.Xna.Framework.Input.ButtonState.Pressed)
+                    {
+                        this.m_Root.m_SpriteBatch.Draw(m_StartenButton[2], m_StartButtonRect, Microsoft.Xna.Framework.Color.White);
+                        this.m_SelectedIndex[0] = true;
+                    }
+                    else
+                    {
+                        this.m_Root.m_SpriteBatch.Draw(m_StartenButton[1], m_StartButtonRect, Microsoft.Xna.Framework.Color.White);
+                    }
+                }
+                else
+                {
+                    this.m_Root.m_SpriteBatch.Draw(m_StartenButton[0], m_StartButtonRect, Microsoft.Xna.Framework.Color.White);
+                }
+                if (Intersects(mousePosition, m_FotowandButtonRect))
+                {
+                    if (this.m_InputManager.GetMouseState().LeftButton == Microsoft.Xna.Framework.Input.ButtonState.Pressed)
+                    {
+                        this.m_Root.m_SpriteBatch.Draw(m_FotowandButton[2], m_FotowandButtonRect, Microsoft.Xna.Framework.Color.White);
+                        this.m_SelectedIndex[1] = true;
+                    }
+                    else
+                    {
+                        this.m_Root.m_SpriteBatch.Draw(m_FotowandButton[1], m_FotowandButtonRect, Microsoft.Xna.Framework.Color.White);
+                    }
+                }
+                else
+                {
+                    this.m_Root.m_SpriteBatch.Draw(m_FotowandButton[0], m_FotowandButtonRect, Microsoft.Xna.Framework.Color.White);
+                }
+                if (Intersects(mousePosition, m_OptionenButtonRect))
+                {
+                    if (this.m_InputManager.GetMouseState().LeftButton == Microsoft.Xna.Framework.Input.ButtonState.Pressed)
+                    {
+                        this.m_Root.m_SpriteBatch.Draw(m_OptionenButton[2], m_OptionenButtonRect, Microsoft.Xna.Framework.Color.White);
+                        this.m_SelectedIndex[2] = true;
+                    }
+                    else
+                    {
+                        this.m_Root.m_SpriteBatch.Draw(m_OptionenButton[1], m_OptionenButtonRect, Microsoft.Xna.Framework.Color.White);
+                    }
+                }
+                else
+                {
+                    this.m_Root.m_SpriteBatch.Draw(m_OptionenButton[0], m_OptionenButtonRect, Microsoft.Xna.Framework.Color.White);
+                }
+                if (Intersects(mousePosition, m_BeendenButtonRect))
+                {
+                    if (this.m_InputManager.GetMouseState().LeftButton == Microsoft.Xna.Framework.Input.ButtonState.Pressed)
+                    {
+                        this.m_Root.m_SpriteBatch.Draw(m_BeendenButton[2], m_BeendenButtonRect, Microsoft.Xna.Framework.Color.White);
+                        this.m_SelectedIndex[0] = true;
+                    }
+                    else
+                    {
+                        this.m_Root.m_SpriteBatch.Draw(m_BeendenButton[1], m_BeendenButtonRect, Microsoft.Xna.Framework.Color.White);
+                    }
+                }
+                else
+                {
+                    this.m_Root.m_SpriteBatch.Draw(m_BeendenButton[0], m_BeendenButtonRect, Microsoft.Xna.Framework.Color.White);
+                }
                 this.m_Root.m_SpriteBatch.End();
             }
+        }
+
+        private bool Intersects(Vector2 _position, Rectangle _rectangle)
+        {
+            bool intersects = false;
+            if (_rectangle.Intersects(new Rectangle((int)_position.X,(int)_position.Y,1,1)))
+            {
+                intersects = true;
+            }
+            return intersects;
         }
     }
 }
