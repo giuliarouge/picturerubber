df7fb5e0500f2b6f3bc669eb122fe40d7d80c5f5
eric.jahn@fotostumpf.de <eric.jahn@fotostumpf.de@efc28e42-3d81-718d-be2e-65ac8396be35>
2011-01-18 10:12:39 +0000

    - deleted usage of PR_Kinect

--- trunk/PictureRubber/PictureRubber/PR_Pictures.cs
+++ trunk/PictureRubber/PictureRubber/PR_Pictures.cs
@@ -32,15 +32,12 @@ namespace PictureRubber
         /// </summary>
         private Texture2D[] m_Textures;
 
-        private PR_Kinect m_Kinect;
-
         /// <summary>
         /// Initializes a new Instance of PR_Pictures
         /// </summary>
-        public PR_Pictures(string _folderPath, PR_Kinect _kinect)
+        public PR_Pictures(string _folderPath)
         {
             this.m_Root = PR_Main.GetInstance();
-            this.m_Kinect = _kinect;
             this.m_FolderPath = _folderPath;
             if (!this.LoadPictures())
             {
@@ -144,18 +141,12 @@ namespace PictureRubber
         public void Draw(GameTime _gameTime)
         {
             this.m_Root.m_SpriteBatch.Begin();
-            Microsoft.Xna.Framework.Rectangle rec;
-            if (this.m_Root.m_Modus == PR_Main.Modus.Debug)
-            {
-                rec = new Microsoft.Xna.Framework.Rectangle(0, 0, this.m_Root.GraphicsDevice.Viewport.Width/2, this.m_Root.GraphicsDevice.Viewport.Height);
-                Microsoft.Xna.Framework.Rectangle depth;
-                depth = new Microsoft.Xna.Framework.Rectangle(this.m_Root.GraphicsDevice.Viewport.Width / 2, 0, this.m_Root.GraphicsDevice.Viewport.Width / 2, this.m_Root.GraphicsDevice.Viewport.Height);
-                this.m_Root.m_SpriteBatch.Draw(this.m_Kinect.GetDepthImageTexture(), depth, Microsoft.Xna.Framework.Color.White);
-            }
-            else
-            {
-                rec = new Microsoft.Xna.Framework.Rectangle(0, 0, this.m_Root.GraphicsDevice.Viewport.Width, this.m_Root.GraphicsDevice.Viewport.Height);
-            }
+            Microsoft.Xna.Framework.Rectangle rec = new Microsoft.Xna.Framework.Rectangle(
+                0,
+                0,
+                this.m_Root.GraphicsDevice.Viewport.Width,
+                this.m_Root.GraphicsDevice.Viewport.Height);
+
             foreach (Texture2D tex in this.m_Textures)
             {
                 if (tex != null)
